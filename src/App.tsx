bff4a508864a1e82e86dd8c538d4e8085e22505b
mohammad <mohammad19hobbi@gmail.com>
2025-05-03 12:18:43 +0330

    feat(task): create TaskItem component with toggle and delete actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import AddTaskForm from "./components/AddTaskForm";
 
 export default function App() {
+  const handleAddTask = (title: string) => {
+    console.log("New task:", title);
+  };
  const [task,setTask]=useState<string[]>([]);
   return (
     <div>
-<AddTaskForm/>    
+<AddTaskForm onAdd={handleAddTask}/>    
     </div>
   )
 }
